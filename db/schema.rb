af157e44f855b0ca026bb50e10bcf314cc0018aa
Stembolt Jenkins <jenkins@stembolt.com>
2016-05-16 15:27:38 +0000

    Update sandbox.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160510193905) do
+ActiveRecord::Schema.define(version: 20160516152942) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",                      null: false
